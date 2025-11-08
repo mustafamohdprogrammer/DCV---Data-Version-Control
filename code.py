03c12896155fd756640128ce97a42a5e9731b0ce
Mustafa <mohdmustafa969@gmail.com>
2025-11-08 14:25:55 +0530

    second version of data

--- code.py
+++ code.py
@@ -9,9 +9,13 @@ data = {
 
 df = pd.DataFrame(data)
 
+new_row_loc = {'Name' : 'David', 'Age' : 28, 'City' : 'Houston'}
+df.loc[len(df.index)] = new_row_loc
+
 
 data_dir = 'data'
-os.mkdir(data_dir)
+if not os.path.exists(data_dir):
+    os.makedirs(data_dir)
 
 file_path = os.path.join(data_dir,'sample.csv')
 df.to_csv(file_path,index=False)
